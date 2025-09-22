5be10b8ec9f09491fb3de61d208e82637bdf7b4b
Filipe Colaquecez <filipepcolaquecez@gmail.com>
2025-09-22 17:18:29 -0300

    chore: known

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+    ];
+  },
   /* config options here */
 };
 
